c2734a04d8ebc94669934bd1e50b33f4fed67f14
Andrei Ciobanu <anciobanu@protonmail.com>
2024-11-03 15:53:42 +0200

    Added basic UDP server

--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+const (
+	UDP  = "udp"
+	port = ":53"
+)
+
+func main() {
+	udpServer, err := net.ListenPacket(UDP, port)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Fatal Error: %v", err))
+	}
+
+	defer udpServer.Close() //inchidem serverul cu defer
+
+	for {
+		buff := make([]byte, 1024) //creeam bufferul in care primim requestul la server
+		_, addr, err := udpServer.ReadFrom(buff)
+		if err != nil {
+			fmt.Println(fmt.Errorf("Error in processing a read: %v", err))
+			continue
+		}
+		//creem un go routine care sa prelucreze separat requestul
+		go handleRequest(udpServer, addr, buff)
+	}
+}
+
+/*
+*	handleRequest: functie care va prelucra reqeustul primit la serverul DNS
+ */
+func handleRequest(udpServer net.PacketConn, addr net.Addr, buff []byte) {}
