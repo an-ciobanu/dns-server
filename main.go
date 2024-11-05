c1f3f67c5ca04df8f2315362e53616053163bf7b
Andrei Ciobanu <anciobanu@protonmail.com>
2024-11-05 08:10:36 +0200

    Added handleRequest function

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 const (
@@ -35,5 +38,35 @@ func main() {
 *	handleRequest: functie care va prelucra reqeustul primit la serverul DNS
  */
 func handleRequest(udpServer net.PacketConn, addr net.Addr, buff []byte) (string, uint16, uint16, error) {
-	return "", uint16(1), uint16(1), nil
+
+	if len(buff) < 12 {
+		//cererea e invalida, nu avem nici macar headerul corect
+		return "", 0, 0, errors.New("Invalid DNS query")
+	}
+
+	//sarim peste header
+	offset := 12
+
+	domainParts := []string{}
+
+	for {
+		//preluam prima lungime de parte din domeniu
+		length := int(buff[offset])
+		if length == 0 {
+			offset++
+			break
+		}
+		offset++
+		//am gasit u bucata de text din domeniu
+		domainParts = append(domainParts, string(buff[offset:offset+length]))
+		offset += length
+	}
+
+	domain := strings.Join(domainParts, ".")
+
+	// Tipul și clasa cererii
+	qType := binary.BigEndian.Uint16(buff[offset : offset+2])
+	qClass := binary.BigEndian.Uint16(buff[offset+2 : offset+4])
+
+	return domain, qType, qClass, nil
 }
